e0e268966c6f9b2291b87dcab5f897d328c93022
robrawler <38539838+robrawler@users.noreply.github.com>
2020-03-04 21:15:16 +0300

    Create task1.pas

---
+++ lw2/task1.pas
@@ -0,0 +1,11 @@
+PROGRAM HttpResponce(INPUT, OUTPUT); 
+USES DOS; 
+BEGIN {HttpResponce} 
+WRITELN('Content-Type: text/plain'); 
+WRITELN; 
+WRITELN(GetEnv('REQUEST_METHOD')); 
+WRITELN(GetEnv('QUERY_STRING')); 
+WRITELN(GetEnv('CONTENT_LENGTH')); 
+WRITELN(GetEnv('HTTP_USER_AGENT')); 
+WRITELN(GetEnv('HTTP_HOST')); 
+END. {HttpResponce}
